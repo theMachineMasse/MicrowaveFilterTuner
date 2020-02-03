17d7b7311f8eb85f9a4b18a4d249d00a10722fae
theMachineMasse <55885611+theMachineMasse@users.noreply.github.com>
2020-02-03 16:18:04 -0600

    STM v1.2
    Added status and error codes to the VDS functions, see google drive doc for info on codes.

--- STM32/VisualDisplaySubSystem.c
+++ STM32/VisualDisplaySubSystem.c
@@ -32,13 +32,21 @@ void vds_Init(void) {
 	GPIOB->CRL |= GPIO_CRL_MODE0 | GPIO_CRL_MODE1 | GPIO_CRL_MODE5;
 	GPIOB->CRL &= ~GPIO_CRL_CNF0 & ~GPIO_CRL_CNF1 & ~GPIO_CRL_CNF5;
 	
-	command_To_LCD(0x38);
-  command_To_LCD(0x38);
-  command_To_LCD(0x38);
-	command_To_LCD(0x38);
-	command_To_LCD(0x0F);
-	command_To_LCD(0x01);
-	command_To_LCD(0x06);
+	/* Start-up Commands */
+	command_To_LCD(0x38); //NO MAGIC NUMBERS
+  command_To_LCD(0x38); //NO MAGIC NUMBERS
+  command_To_LCD(0x38); //NO MAGIC NUMBERS
+	command_To_LCD(0x38); //NO MAGIC NUMBERS
+	command_To_LCD(0x0F); //NO MAGIC NUMBERS
+	command_To_LCD(0x01); //NO MAGIC NUMBERS
+	command_To_LCD(0x06); //NO MAGIC NUMBERS
+	
+	/* Branding */
+	string_To_LCD("University of Regina");
+	command_To_LCD(0xC0);
+	string_To_LCD("MFT v1.0");
+	//add code to set to line 3
+	string_To_LCD("Status: ");
 }
 
 
@@ -52,8 +60,8 @@ void vds_Init(void) {
 *******************************************/
 void data_To_LCD (uint8_t data) {
 	GPIOB->BSRR = LCD_DM_ENA; //RS high, E high
-	GPIOC->ODR &= 0xFF00; //GOOD: clears the low bits without affecting high bits
-	GPIOC->ODR |= data; //GOOD: only affects lowest 8 bits of Port C
+	GPIOC->ODR &= 0xFF00; //NO MAGIC NUMBERS
+	GPIOC->ODR |= data; 
 	delay(8000);
 	GPIOB->BSRR = LCD_DM_DIS; //RS high, E low
 	delay(80000);
@@ -70,8 +78,8 @@ void data_To_LCD (uint8_t data) {
 *******************************************/
 void command_To_LCD (uint8_t data) {
 	GPIOB->BSRR = LCD_CM_ENA; //RS low, E high
-	GPIOC->ODR &= 0xFF00; //GOOD: clears the low bits without affecting high bits
-	GPIOC->ODR |= data; //GOOD: only affects lowest 8 bits of Port C
+	GPIOC->ODR &= 0xFF00; //NO MAGIC NUMBERS
+	GPIOC->ODR |= data; 
 	delay(8000);
 	GPIOB->BSRR = LCD_CM_DIS; //RS low, E low
 	delay(80000);
@@ -106,11 +114,38 @@ void string_To_LCD (char * message) {
 *	Return value: N/A
 *******************************************/
 void lcd_Display_Status (int statusCode) {
-	
+
 	switch(statusCode) {
 		case 0: 
+			string_To_LCD("Status: Disconnected");
 			break;
-		
+		case 1: 
+			string_To_LCD("Status: Initializing");
+			break;
+		case 2: 
+			string_To_LCD("Status: Ready");
+			break;
+		case 3: 
+			string_To_LCD("Status: Homing X,Y");
+			break;
+		case 4: 
+			string_To_LCD("Status: Homing Phi");
+			break;
+		case 5: 
+			string_To_LCD("Status: Moving X");
+			break;
+		case 6: 
+			string_To_LCD("Status: Moving Y");
+			break;
+		case 7: 
+			string_To_LCD("Status: Moving Z");
+			break;
+		case 8: 
+			string_To_LCD("Status: Moving S");
+			break;
+		default: 
+			string_To_LCD("Status: Unknown");
+			break;
 	}	
 }
 
@@ -124,12 +159,56 @@ void lcd_Display_Status (int statusCode) {
 *	Return value: N/A
 *******************************************/
 void lcd_Display_Error (int errorCode) {
+//add code to set cursor to line 4
 	
 	switch(errorCode) {
-		case 0: 
+		case 11: 
+			string_To_LCD("Error: 1-1 g_code");
 			break;
-		
+		case 12: 
+			string_To_LCD("Error: 1-2 no screw");
+			break;
+		case 13: 
+			string_To_LCD("Error: 1-3 encoder");
+			break;
+		case 14: 
+			string_To_LCD("Error: 1-4 X home");
+			break;
+		case 15: 
+			string_To_LCD("Error: 1-5 Y home");
+			break;
+		case 21: 
+			string_To_LCD("Error: 2-1 no resp");
+			break;
+		case 22: 
+			string_To_LCD("Error: 2-2 no screw");
+			break;
+		case 23: 
+			string_To_LCD("Error: 2-3 F Y hit");
+			break;
+		case 24: 
+			string_To_LCD("Error: 2-4 B Y hit");
+			break;
+		case 25: 
+			string_To_LCD("Error: 2-5 L X hit");
+			break;
+		case 26: 
+			string_To_LCD("Error: 2-6 R Y hit");
+			break;
+		case 27: 
+			string_To_LCD("Error: 2-7 U Z hit");
+			break;
+		case 28: 
+			string_To_LCD("Error: 2-8 L Z hit");
+			break;
+		case 31: 
+			string_To_LCD("Error: 3-1 g_code");
+			break;
+		default: 
+			string_To_LCD("Error: Unknown");
+			break;
 	}
+//add code to set cursor back to line 3
 }
 
 /*******************************************
--- STM32/VisualDisplaySubSystem.h
+++ STM32/VisualDisplaySubSystem.h
@@ -14,17 +14,17 @@
 
 
 /* DEFINES */
-#define LCD_CM_ENA 0x00210002 
+#define LCD_CM_ENA 0x00210002  
 #define LCD_CM_DIS 0x00230000 
 #define LCD_DM_ENA 0x00200003 
 #define LCD_DM_DIS 0x00220001 
 
-#define LCD_8B2L 0x38 // ; Enable 8 bit data, 2 display lines
-#define LCD_DCB 0x0F // ; Enable Display, Cursor, Blink
-#define LCD_MCR 0x06 // ; Set Move Cursor Right
-#define LCD_CLR 0x01 // ; Home and clear LCD
-#define LCD_LN1 0x80 // ;Set DDRAM to start of line 1
-#define LCD_LN2 0xC0 // ; Set DDRAM to start of line 2
+#define LCD_8B2L 0x38 // Enable 8 bit data, 2 display lines
+#define LCD_DCB 0x0F //  Enable Display, Cursor, Blink
+#define LCD_MCR 0x06 //  Set Move Cursor Right
+#define LCD_CLR 0x01 //  Home and clear LCD
+#define LCD_LN1 0x80 //  Set DDRAM to start of line 1
+#define LCD_LN2 0xC0 //  Set DDRAM to start of line 2
 
 
 /* FUNCTIONS */
