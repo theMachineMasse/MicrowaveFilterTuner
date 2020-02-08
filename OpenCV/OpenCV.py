625fb01c68a274bdef4694b268e7ec09e55c6a2f
theMachineMasse <55885611+theMachineMasse@users.noreply.github.com>
2020-02-08 17:31:32 -0600

    1.9 - Added Post Processing Functionality
    Added functionality to display a clean, resized image that does not include non-selected screws. Need to work with Matt and Jarett to incorporate buttons on the screw selection window. Also need to finish implementation of dynamic photo capturing.

--- OpenCV/OpenCV.py
+++ OpenCV/OpenCV.py
@@ -4,7 +4,7 @@
 # Date: January 18, 2020
 # Programmer(s): Braden Massé
 # Sub-Systems: Visual Identification Sub-System
-# Version: 1.8
+# Version: 1.9
 ##############################################
 
 # Import Libraries #
@@ -16,7 +16,8 @@ screwLocationsGList = []  # (screwNum, assignedNum, x, y, z, r, locatedFlag)
 g_minDepth = 260  # the distance from the tallest tuning screw (mm)
 wideCamPort = 1  # COM port for the wide angle camera
 g_screwNum = 0  # counter for screw assignment
-g_screwCount = 0  # number of screws detected
+#g_screwCount = 0  # number of screws detected
+screwsDetected = 0  # flag that is set if any screws detected
 
 ##############################################
 # Function: wide_Angle_Camera()
@@ -27,21 +28,23 @@ g_screwCount = 0  # number of screws detected
 # Outputs: N/A
 ##############################################
 def wide_Angle_Camera(sensitivityVal):
-    # Variable Initializations #
+    # Globals #
     global g_minDepth
     global g_screwCount
-    screwLocations = []  # will be appended to global screw positions list
+    global screwsDetected
+
+    # Variable Initializations #
     convertFactor = 38  # number of pixels per cm, needs tuning
     dp = 1
-    minDist = 100
-    upCannyThres = sensitivityVal * 2
-    centerThres = sensitivityVal
-    maxR = 40
-    minR = 30
-    screwCount = 0
-    measuredDepth = 210  # units are mm
-    screwDiameter = 10  # units are mm
-    referenceRadius = 38  # units are pixels
+    minDist = 100  # allowable distance between screws
+    upCannyThres = sensitivityVal * 2  # updated based on passed parameter
+    centerThres = sensitivityVal  # updated based on passed parameter
+    maxR = 40  # max radius of screw, update based on GUI selection
+    minR = 30  # min radius of screw, update based on GUI selection
+    screwCount = 0  # counter for number of screws detected
+    measuredDepth = 210  # units are mm, used for calibrating depth
+    screwDiameter = 10  # units are mm, used for calibrating depth
+    referenceRadius = 38  # units are pixels, used for calibrating depth
     xUpperFilterMargin = 500  # needs to change depending on GUI selection
     yUpperFilterMargin = 500  # needs to change depending on GUI selection
     xLowerFilterMargin = 500  # needs to change depending on GUI selection
@@ -55,33 +58,45 @@ def wide_Angle_Camera(sensitivityVal):
     # cap.release()
 
     # Open Image #
-    #img = cv2.imread('negative_test.jpg')  # no screw testing, comment out if taking picture
+    # img = cv2.imread('negative_test.jpg')  # no screw testing, comment out if taking picture
     img = cv2.imread('opencv_frame_0.png')  # no screw testing, comment out if taking picture
 
     # Locating of Screws (x, y) #
     output = img.copy()
+    temp = img.copy()
+    cv2.imwrite('output.png', output)  # use for displaying image on GUI
+
+    # Get Image Dimensions #
+    xWidth = img.shape[1]
+    yWidth = img.shape[0]
+
+    # Resize and Save Temporary Image #
+    x3 = int(xWidth / 2)
+    y3 = int(yWidth / 2)
+    temp = cv2.resize(temp, (x3, y3))
+    cv2.imwrite('temp.png', temp)  # use for displaying image on GUI
+
+    # Search Image For Screws #
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # change to greyscale image
     gray = cv2.medianBlur(gray, 5)
-    circles = []
     circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp, minDist, param1=upCannyThres, param2=centerThres, minRadius=minR, maxRadius=maxR)
 
     # No Screws Located #
     if circles is None:
+        screwsDetected = 0  # set flag to false if no screws detected
         screwLocations = [-1, -1, -1, -1, -1, -1, -1]
         screwLocationsGList.append(screwLocations)
         print("No screws detected")  # testing, will need message to appear on GUI
 
     # Screws Located *
     else:
-        # Get Image Dimensions #
-        xWidth = img.shape[1]
-        yWidth = img.shape[0]
-
         detected_circles = np.uint16(np.around(circles))
+        screwsDetected = 1  # set flag to true if screws detected
 
         # Update Tuning Screw List #
         focalLength = ((referenceRadius * 2) * measuredDepth) / screwDiameter  # determine focal length
         for (x, y, r) in detected_circles[0, :]:
+
             # Find Depth of Current Screw #
             calculatedDepth = (screwDiameter * focalLength) / (r * 2)
 
@@ -101,14 +116,21 @@ def wide_Angle_Camera(sensitivityVal):
                 cv2.circle(output, (x, y), 2, (0, 255, 0), 3)  # draw dot on center of detected screws
                 screwCount = screwCount + 1
 
-        g_screwCount = screwCount
+        #g_screwCount = screwCount
+
         # Testing Outputs #
         print("Minimum Depth is", g_minDepth, "mm")  # testing
         print("There are", screwCount, "screws")  # testing
         print("Focal length is", focalLength, "mm")  # testing
         print(screwLocationsGList)
+
+        # Resize Output #
+        output = cv2.resize(output, (x3, y3))
+
+        # Save and Show Output Image #
         cv2.imwrite('output.png', output)  # use for displaying image on GUI
-        cv2.imshow('output', output)  # display output with screws identified, needs to be integrated into GUI
+        # cv2.imshow('output', output)  # display output with screws identified, needs to be integrated into GUI
+
     # End of Function Clean-Up *
     cv2.waitKey(0)  # close image on pressing any key
     cv2.destroyAllWindows()
@@ -126,38 +148,41 @@ def click_event(event, x, y, flags, param):
     font = cv2.FONT_HERSHEY_COMPLEX
     validFlag = 0
     convertFactor = 38  # number of pixels per cm, needs tuning
-    fontScale = 1
+    fontScale = 0.9
     fontThickness = 2
 
     # Open Image #
     img2 = cv2.imread('output.png')
+    temp = cv2.imread('temp.png')
 
     # Left Click Event #
     if event == cv2.EVENT_LBUTTONDOWN:
         for i in range(len(screwLocationsGList)):
 
             # Find Needed Values from List #
-            x1 = screwLocationsGList[i][2]*convertFactor
-            r1 = screwLocationsGList[i][5]
-            y1 = screwLocationsGList[i][3]*convertFactor
+            x1 = int((screwLocationsGList[i][2]*convertFactor) / 2)
+            r1 = int(screwLocationsGList[i][5] / 2)
+            y1 = int((screwLocationsGList[i][3]*convertFactor) / 2)
             assignedNum = screwLocationsGList[i][1]
 
             # Check Click Location Is Within A Screw #
             if (x1 - r1) < x < (x1 + r1) and (y1 - r1) < y < (y1 + r1) and assignedNum == -1:
                 screwLocationsGList[i][1] = g_screwNum
+                strXY = str(g_screwNum)  # convert number to a string
+                g_screwNum = g_screwNum + 1
                 # print("Screw identified, screw = ", i)  # testing
-                validFlag = 1
-                break
 
-        # If Valid Screw Clicked, Add Screw Number To Image #
-        if validFlag == 1:
-            strXY = str(g_screwNum)
-            # strXY = str(x) + ", " + str(y) # testing
-            g_screwNum = g_screwNum + 1
-            cv2.putText(img2, strXY, (x, y), font, fontScale, (255, 255, 0), fontThickness)
-            cv2.imshow('Screw Assignment', img2)
-            cv2.imwrite('output.png', img2)  # use for displaying image on GUI
-            # print(screwLocationsGList)  # testing
+                # If Valid Screw Clicked, Add Screw Number To Image #
+                cv2.circle(temp, (x1, y1), r1, (0, 255, 0), 2)  # draw circles on detected screws
+                cv2.putText(temp, strXY, (int(x1-r1/2), int(y1+r1/2)), font, fontScale, (255, 255, 0), fontThickness)  # add screw number to image
+                cv2.putText(img2, strXY, (x, y), font, fontScale, (255, 255, 0), fontThickness)
+
+                # Display and Save New Output #
+                cv2.imshow('Screw Assignment', img2)
+                cv2.imwrite('output.png', img2)  # use for displaying image on GUI
+                cv2.imwrite('temp.png', temp)  # use for displaying image on GUI
+
+    # print(screwLocationsGList)  # testing
 
 ##############################################
 # Function: screw_Assignment()
@@ -168,8 +193,11 @@ def click_event(event, x, y, flags, param):
 # Outputs: N/A
 ##############################################
 def screw_Assignment():
-    # Variable Initializations #
+    # Globals #
     global screwLocationsGList
+    global g_screwNum
+
+    # Variable Initializations #
     tempScrewLocationsList = []
 
     # Open & Display Image #
@@ -186,6 +214,7 @@ def screw_Assignment():
     # Check If Any Screws Not Selected; Copy Any With Assigned Numbers #
     for i in range(len(screwLocationsGList)):
         assignedNum = screwLocationsGList[i][1]
+
         if assignedNum != -1:
             tempScrewLocationsList.append(screwLocationsGList[i])
 
@@ -194,6 +223,17 @@ def screw_Assignment():
     print(screwLocationsGList)  # testing
     print("New List Length is", len(screwLocationsGList))  # testing
 
+    temp = cv2.imread('temp.png')  # testing
+    cv2.imwrite('output.png', temp)  # use for displaying image on GUI
+    cv2.imshow('Screw Assignment', temp)  # testing
+
+    # Close Window #
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 wide_Angle_Camera(25)  # testing
-screw_Assignment()  # testing
+
+# To Be Incorporated Into The GUI #
+if screwsDetected != 0:
+    screw_Assignment()  # testing
