94994dc317bda91ba619fd6348fefb5188ab8630
m-rostad <rostad.mattman@gmail.com>
2020-03-14 11:39:42 -0600

    Basic Python to STM communications working

--- Python/MicrowaveFilterTuningBackend.py
+++ Python/MicrowaveFilterTuningBackend.py
@@ -1,12 +1,58 @@
 import serial
 
-comPort = 'COM5'
+comPort = 'COM7'
 
-ser = serial.open(comPort)
-ser.flushInput()
 
-ser.write('M17')
-while(ser.readLine() )
+def automaticTune():
+    ser = serial.Serial(comPort, 9600, timeout=0.01)
+    ser.flushInput()
+    line = b''
 
-ser.write('g28')
+    ser.write(b'm17')
+    ser.write(b'\r')
+    while b'k' not in line :
+        line = ser.readline()
+    print('k')
 
+
+    line = b''
+    ser.write(b'g28')
+    ser.write(b'\r')
+    while b'k' not in line :
+        line = ser.readline()
+    print('k')
+
+
+    line = b''
+    ser.write(b'g0 x100 y100')
+    ser.write(b'\r')
+    while b'k' not in line :
+        line = ser.readline()
+    print('k')
+
+    line = b''
+    ser.write(b'g0 x135 y200 z50')
+    ser.write(b'\r')
+    while b'k' not in line :
+        line = ser.readline()
+    print('k')
+
+    line = b''
+    ser.write(b'g16 p720 n720')
+    ser.write(b'\r')
+    while b'k' not in line:
+        line = ser.readline()
+    print('k')
+
+    line = b''
+    ser.write(b'm18')
+    ser.write(b'\r')
+    while b'k' not in line :
+        line = ser.readline()
+    print('k')
+
+
+    ser.close()
+
+
+automaticTune()
